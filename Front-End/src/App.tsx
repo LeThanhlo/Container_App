b3775decafae4f5893bd9443cef4582cb34cc997
TuanNguyenGomi <longle@gomicorp.vn>
2024-10-08 17:10:09 +0700

    frontend

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,7 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MenuLeft from "./components/menuleft/MenuLeft";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <MenuLeft />
+    </div>
+  );
 };
 
 export default App;
